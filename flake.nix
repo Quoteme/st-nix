7d366db940baef8462e06b1ddcbdaaaae8d0bb8e
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-02-18 10:23:35 +0100

    trying to still fix 'builtins.storePath' is not allowed in pure evaluation mode

--- flake.nix
+++ flake.nix
@@ -3,42 +3,53 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = github:edolstra/flake-compat;
       flake = false;
     };
   };
 
-  outputs = {self, nixpkgs, flake-compat}: 
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation rec {
-      version = "0.8.4";
-      pname = "st-nix";
-      src = ./.;
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-        ncurses
-        fontconfig
-        freetype
-      ];
-      buildInputs = with pkgs; [
-        xorg.libX11
-        xorg.libXft
-        harfbuzz # for ligature support
-        scientifica
-        fira-code
-        hasklig
-      ];
-      TERMINFO=".";
-      makeFlags = [
-        "DESTDIR=$(out)"
-        "PREFIX="
-      ];
-      postInstall = ''
-        cp $out/bin/st $out/bin/st-nix
-      '';
-    };
-  };
+  outputs = {self, nixpkgs, flake-compat, flake-utils}: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in rec {
+        packages.st-nix = pkgs.stdenv.mkDerivation rec {
+          version = "0.8.4";
+          pname = "st-nix";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            ncurses
+            fontconfig
+            freetype
+          ];
+          buildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libXft
+            harfbuzz # for ligature support
+            scientifica
+            fira-code
+            hasklig
+          ];
+          TERMINFO=".";
+          makeFlags = [
+            "DESTDIR=$(out)"
+            "PREFIX="
+          ];
+          # This causes an error: 'builtins.storePath' is not allowed in pure evaluation mode
+          # postInstall = ''
+          #   cp $out/bin/st $out/bin/st-nix
+          # '';
+        };
+        defaultPackage = packages.st-nix;
+        defaultApp = {
+          type = "app";
+          program = "${defaultPackage}/bin/st";
+        };
+      }
+    );
 }
